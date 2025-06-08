b3961b6f12f1bcc22299b12b2de7507c8b3e0e6f
Amaan_Malik <amanmalik12081994@gmail.com>
2025-06-08 20:02:36 +0530

    changes in base file

--- src/test/java/Swag_Labs/BaseClass.java
+++ src/test/java/Swag_Labs/BaseClass.java
@@ -2,6 +2,7 @@ package Swag_Labs;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,6 +12,16 @@ public class BaseClass {
 	
 	@BeforeMethod
 	public void SetUp() {
+String BrowserName = System.getenv("Browser");
+		
+		if(BrowserName.equalsIgnoreCase("firefox")) 
+		{
+			driver = new FirefoxDriver();
+		}
+		else 
+		{
+			driver = new ChromeDriver();
+		}
 		
 		driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
